505ceac294d708afb3d71b0d99191b64ad75687c
PC <PC@DESKTOP-VO4HEPR>
2021-06-21 21:22:50 +0900

    2nd master version

--- MyGitJava/src/A.java
+++ MyGitJava/src/A.java
@@ -2,5 +2,9 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("this line printed from A");
+		B classB = new B();
+		classB.printB();
+		C classC = new C();
+		classC.printC();
 	}
 }
